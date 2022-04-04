e7e81a17c7a5947dfb47e2244367ebd6359410eb
iyappan5 <101567482+iyappan5@users.noreply.github.com>
2022-04-04 22:48:01 +0530

    Create CalculatorUsing_if.py

---
+++ CalculatorUsing_if.py
@@ -0,0 +1,11 @@
+a=int(input("enter a :"))
+b= int(input("enter b:"))
+opr=input("enter opr:")
+if opr=="+":
+    print(a+b)
+if opr=="-":
+    print(a-b)
+if opr=="*":
+    print(a*b)
+if opr=="/":
+    print(a/b)
